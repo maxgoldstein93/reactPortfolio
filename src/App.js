812eb5df10c94d2314fbe09f6062240d93552e01
Max Goldstein <magoldstein93@gmail.com>
2020-11-06 14:53:11 -0500

    updated router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router, Route } from "react-router-dom";
 import Navbar from "./components/navbar";
 import Footer from "./components/footer"
 import About from "./components/About/about"
--- src/components/footer.js
+++ src/components/footer.js
@@ -20,7 +20,7 @@ function Footer() {
                 <div className="row m-auto">
                     <a className="nav-link" href="https://www.linkedin.com/in/max-goldstein-11a33396/" target="_blank"><i style={styles} className="fab fa-linkedin fa-2x"></i></a>
                     <a className="nav-link" href="https://github.com/maxgoldstein93" target="_blank"><i style={styles} className="fab fa-github fa-2x"></i></a>
-                    <a className="nav-link" href="https://docs.google.com/document/d/e/2PACX-1vSgV67Zj3o5KY0pUWQUVf3qwFZ5tHWsSBJ-A_slNGhqYgeq5uLVbIPTY8MimwcCii21JTxv5HW7C3_R/pub" target="_blank"><i style={styles} className="far fa-file fa-2x"></i></a>
+                    <a className="nav-link" href="https://github.com/maxgoldstein93/reactPortfolio/blob/main/public/assets/Max_Goldstein_October_Resume.pdf?raw=true" target="_blank"><i style={styles} className="far fa-file fa-2x"></i></a>
                     <a className="nav-link" href="mailto:magoldstein93@gmail.com" target="_blank"><i style={styles} className="far fa-envelope fa-2x"></i></a>
                     <a className="nav-link" href="tel:4124170884" ><i  style={styles} className="fas fa-mobile-alt fa-2x"></i></a>
                    
