cb83303056fe359fbe1ac4dadbcf0ebb61bb698e
Max Goldstein <magoldstein93@gmail.com>
2020-11-06 16:01:15 -0500

    added resume

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { HashRouter as Router, Route } from "react-router-dom";
 import Navbar from "./components/navbar";
-import Footer from "./components/footer"
+import Footer from "./components/footer/footer"
 import About from "./components/About/about"
 import Contact from "./components/contact"
 import Portfolio from "./components/portfolio"
@@ -16,10 +16,9 @@ function App() {
     <Router>
       <div >
         <Navbar />
-        
+        <Route exact path = "/" component ={About} />
         <Route exact path = "/about" component = {About} />
         <Route exact path = "/contact" component = {Contact} />
-        
         <Route exact path = "/portfolio" component = {Portfolio} />
         
         
--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -20,7 +20,7 @@ function Footer() {
                 <div className="row m-auto">
                     <a className="nav-link" href="https://www.linkedin.com/in/max-goldstein-11a33396/" target="_blank"><i style={styles} className="fab fa-linkedin fa-2x"></i></a>
                     <a className="nav-link" href="https://github.com/maxgoldstein93" target="_blank"><i style={styles} className="fab fa-github fa-2x"></i></a>
-                    <a className="nav-link" href="https://github.com/maxgoldstein93/reactPortfolio/blob/main/public/assets/Max_Goldstein_October_Resume.pdf?raw=true" target="_blank"><i style={styles} className="far fa-file fa-2x"></i></a>
+                    <a className="nav-link" href="https://drive.google.com/file/d/1bzMQ4plUtkiLCAkzJUYMm3so6KJZjpO5/view?usp=sharing" target="_blank"><i style={styles} className="far fa-file fa-2x"></i></a>
                     <a className="nav-link" href="mailto:magoldstein93@gmail.com" target="_blank"><i style={styles} className="far fa-envelope fa-2x"></i></a>
                     <a className="nav-link" href="tel:4124170884" ><i  style={styles} className="fas fa-mobile-alt fa-2x"></i></a>
                    
